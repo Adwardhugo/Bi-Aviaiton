cf1eda98c69622521fea4b5be00442c6ac07c7bb
adwardhugo <adwardhugo@wifi-172-23-59-167.uoa-wifi.auckland.ac.nz>
2026-01-30 16:41:19 +1300

    aircraftlist backend finish

---
+++ backend/bi-avaition-backend/src/main/java/org/example/biavaitionbackend/mapper/AircraftMapper.java
@@ -0,0 +1,16 @@
+package org.example.biavaitionbackend.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.example.biavaitionbackend.pojo.Aircraft;
+
+import java.util.List;
+
+@Mapper
+public interface AircraftMapper {
+
+    public List<Aircraft> findAll( String registrationNo,
+                                   String serialNumber,
+                                   String operatorCode,
+                                   String aircraftStatus);
+}
